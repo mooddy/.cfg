7705e4bcda39b4036f90f65883b6a38aed9c3e74
mooddy <mooddyshrestha90@gmail.com>
2022-12-04 19:59:32 +0545

    learn bare

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -16,36 +16,41 @@ import XMonad.Hooks.EwmhDesktops
 
 import XMonad.Layout.Combo
 import XMonad.Layout.Square
+import XMonad.Util.Loggers
 
 main :: IO()
 main = xmonad
      .ewmhFullscreen
      .ewmh
      .withEasySB (statusBarProp "polybar" (pure myPolybarPP)) defToggleStrutsKey
+--     .withEasySB (statusBarProp "xmobar" (pure myXmobarPP)) defToggleStrutsKey   
      $ myConfig
 
 
 myConfig = def
     { modMask = mod4Mask
     , layoutHook = spacingRaw True (Border 0 3 0 0) True (Border 0 3 0 0) True $ myLayout
-    , terminal = "urxvt"
     , borderWidth = 3
-    , focusedBorderColor = "#004EFF" 
+    , terminal = "urxvt"
+    , focusedBorderColor = "#FF0000" 
+    , normalBorderColor = "#000000"
     , startupHook = setDefaultCursor xC_left_ptr
     }
   `additionalKeysP`
    [ ("M-f",        spawn "apulse firefox")
-   , ("M-s",        spawn "tabbed -c surf -e")
+   , ("M-s",        spawn "surf google.com")
    , ("M-g",        spawn "google-chrome")
+   , ("M-r",        spawn "brave-browser --incognito")
    , ("M-C-s", unGrab *> spawn "scrot -s")
+   , ("M-t",        spawn "telegram-desktop")
    ]    
 
-myLayout = tiled ||| Mirror tiled ||| threeCol ||| Full ||| noBorders Full
+myLayout = Mirror tiled ||| tiled ||| threeCol ||| Full ||| noBorders Full
   where
     threeCol = ThreeColMid nmaster delta ratio
     tiled    = Tall nmaster delta ratio
     nmaster  = 1 --Number of master pane
-    delta    = 3/100 -- %of screeni to incremnet when resizing panesa
+    delta    = 3/100 -- %of screeni to incremnet when resizing panes
     ratio    = 1/2  --Default porpotion of the screen occupied by the master pane
 
 
